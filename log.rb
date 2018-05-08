5c6554b299d5fbec36e024daadc028ac37d6af5f
Huw Taylor <HuwTaylor@CrispThinking.com>
2018-05-08 16:48:55 +0100

    added better verbs

--- log.rb
+++ log.rb
@@ -1,12 +1,12 @@
 class Logger
 
-    NONE        = 0
-    FATAL       = 1
-    ERROR       = 2
-    WARNING     = 3
-    INFORMATION = 4
-    DEBUG       = 5
-    TRACE       = 6
+    NONE    = 0
+    FATAL   = 1
+    ERROR   = 2
+    WARNING = 3
+    INFO    = 4
+    DEBUG   = 5
+    TRACE   = 6
 
     PRIORITY_STRINGS = %w(None Fatal Error Warning Info Debug Trace)
 
@@ -27,25 +27,45 @@ class Logger
         @importance_level = level
     end
 
-    def push(message, importance=INFORMATION)
-        log_message(message, importance)
+    def push(message, importance=INFO)
+        log(message, importance)
         @@depths[@out] += 1
     end
 
-    def pop(message, importance=INFORMATION)
+    def pop(message, importance=INFO)
         @@depths[@out] -= 1
         @@depths[@out] = 0 if @@depths[@out] < 0
-        log_message(message, importance)
+        log(message, importance)
     end
 
-    def log(message, importance=INFORMATION)
+    def trace(message)
+        log(message, TRACE)
+    end
+    def debug(message)
+        log(message, DEBUG)
+    end
+    def info(message)
+        log(message, INFO)
+    end
+    def warn(message)
+        log(message, WARNING)
+    end
+    def error(message)
+        log(message, ERROR)
+    end
+    def fatal(message)
+        log(message, FATAL)
+    end
+
+    def log(message, importance=INFO)
         log_message(message, importance)
     end
 
     def log_message(message, importance)
         return if importance > @importance_level
-        @out.puts "[#{@source}] (#{PRIORITY_STRINGS[importance]}): " + (" " * @padding * @@depths[@out]) + message 
         @last_message = message
+        @out.puts "[#{@source}] (#{PRIORITY_STRINGS[importance]}): " + (" " * @padding * @@depths[@out]) + message 
+        @out.flush
     end
 
 end
